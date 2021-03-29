41055d96ef96feaf4c701ead17d0569962891593
Florian <florian@mail.com>
2021-03-29 19:41:24 +0200

    Ajout d'une condition pour les paramètres de l'url
    Signed-off-by: Florian <florian@mail.com>

--- Front-end/JS/confirmation.js
+++ Front-end/JS/confirmation.js
@@ -1,21 +1,32 @@
 const searchParams = new URLSearchParams(window.location.search);
 
 // Récupérer les paramètres GET de l'url
-const orderID = searchParams.get('inputOrderId');
-const price = searchParams.get('inputPrice');
-const firstName = searchParams.get('prenom');
+const searchOrderIDr = searchParams.has('inputOrderId');
+const searchPrice = searchParams.has('inputPrice');
+const searchFirstName = searchParams.has('prenom');
 
-// Ajoute les valeurs des paramètres dans différentes balises HTML
-const displayOrderID = document.getElementById('ID');
-const displayPrice = document.getElementById('prix');
-const displayFirstName = document.getElementById('prenom');
-displayOrderID.innerHTML = orderID;
-displayPrice.innerHTML = price;
-displayFirstName.innerHTML = firstName;
-
-const panier = localStorage.getItem('panier');
-
-if (panier) 
+if (!searchOrderIDr || !searchPrice || !searchFirstName) 
 {
-    localStorage.removeItem('panier');
+    window.location.href = "http://127.0.0.1:5501/Front-end/index.html"
+}
+else
+{
+    const orderID = searchParams.get('inputOrderId');
+    const price = searchParams.get('inputPrice');
+    const firstName = searchParams.get('prenom');
+
+    // Ajoute les valeurs des paramètres dans différentes balises HTML
+    const displayOrderID = document.getElementById('ID');
+    const displayPrice = document.getElementById('prix');
+    const displayFirstName = document.getElementById('prenom');
+    displayOrderID.innerHTML = orderID;
+    displayPrice.innerHTML = price;
+    displayFirstName.innerHTML = firstName;
+
+    const panier = localStorage.getItem('panier');
+
+    if (panier) 
+    {
+        localStorage.removeItem('panier');
+    }  
 }
